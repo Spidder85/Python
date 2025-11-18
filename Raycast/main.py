3417fc07beb1e90d02dbf906b459470e70a9a2e1
lharyshu <Igor@lharyshu>
2025-11-18 22:54:41 +0300

    first commit

---
+++ Raycast/main.py
@@ -0,0 +1,32 @@
+import pygame
+from settings import *
+from map import Map
+from player import Player
+from raycaster import Raycaster
+
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+map = Map()
+player = Player()
+
+clock = pygame.time.Clock()
+raycaster = Raycaster(player)
+
+while True:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    
+    player.update()
+    raycaster.castAllRays()
+
+    screen.fill((0, 0, 0))
+
+    map.render(screen)
+    player.render(screen)
+
+    raycaster.render(screen)
+
+    pygame.display.update()
---
+++ Raycast/raycaster.py
@@ -0,0 +1,23 @@
+import pygame
+from settings import *
+from ray import Ray
+
+class Raycaster:
+    def __init__(self, player):
+        self.rays = []
+        self.player = player
+
+    def castAllRays(self):
+        self.rays = []
+        rayAngle = self.player.rotationAngle - FOV / 2
+        for i in range(NUM_RAYS):
+            ray = Ray(rayAngle, self.player)
+            ray.cast()
+            self.rays.append(ray)
+
+            rayAngle += FOV / NUM_RAYS
+
+
+    def render(self, screen):
+        for ray in self.rays:
+            ray.render(screen)
